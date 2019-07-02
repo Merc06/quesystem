2904ecf79420de275b315b3e3bfad0a456b5680b
merc06 <merclouie0506@yahoo.com>
2019-07-02 08:28:11 -0700

    push'

---
+++ resources/js/components/Dashboard.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-body table-responsive">
+                    <table class="table table-bordered text-center">
+                        <tr>
+                            <td>
+                                <div class="border rounded-circle que">
+                                    <h1 class="display-1">1234</h1>
+                                </div>
+                            </td>
+
+                            <td>
+                                <h2>Marc</h2>
+                                <h2>Marc</h2>
+                                <h2>Marc</h2>
+                                <h2>Marc</h2>
+                            </td>
+                        </tr>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.que {
+    height: 20rem;
+    width: 20rem;
+}
+</style>
